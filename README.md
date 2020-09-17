1700ab5057e636ea7ceb1ffedc3bfd47746548f6
XabierGallardo <javiergallard91@gmail.com>
2020-09-17 13:50:58 +0200

    Turning new combined password arrays onto string and printing it to a txt file

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ Select both **lenght** and **type** buttons and click on **Generate** to create 
 
 <img src="sample.PNG" alt="Web Sample Image">
 
-### Dictionary creator
+# Dictionary creator
 Dictionary creator allows you to create standarized WPA Dictionaries.
 You must run this from **Linux Terminal**. 
-- On Linux :penguin: Use Terminal *Ctrl + Alt + T*
-- On Windows :computer: Use WSL *Windows Subsystem for Linux*
-- On Mac :apple: Use Terminal *Command + Space* and type Terminal
+- On **Linux** :penguin: 	Use Terminal *Ctrl + Alt + T*
+- On **Windows** :computer: 	Use WSL *Windows Subsystem for Linux*
+- On **Mac** :apple: 	Use Terminal *Command + Space* and type Terminal
 
 ```sh
 #Remember to add permissions on dictionaryCreator.sh
--- dictionaryCreator.sh
+++ dictionaryCreator.sh
@@ -5,13 +5,28 @@ words=(A a B b C c D d E e F f G g H h I i J j K k L l M m N n O o P p Q q R r S
 numbers=(0 1 2 3 4 5 6 7 8 9)
 combined=("${words[@]}" "${numbers[@]}")
 
-# Read
-for (( x=0; x<10; x++)); do
+newArray=()
+#dictionary=/Dictionaries/dictionary.txt
 
-	# Generate random number according to its array length
-	arrLength=${#combined[@]}
-	randomChar=$((0 + RANDOM % arrLength))
-	newPass=(x)
-done
+	for (( x=0; x<10; x++)); do
 
+		# Generate random number according to its array length
+		arrLength=${#combined[@]}
+		randomChar=$((0 + RANDOM % arrLength))
+
+		# Create new character based on random location and add onto array
+		newChar=( ${combined[randomChar]} )
+		newArray+=($newChar)
+	done
+
+# Copy array onto an string and remote spaces
+printf -v newPass '%s' "${newArray[@]}"
+newPass=${newPass:1}
 echo $newPass
+
+
+echo $newPass >> test.txt
+
+#if [ -f "$dictionary" ]; then
+#	cat <<< "$test" > "$dictionary"
+#fi
